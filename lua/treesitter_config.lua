c923f782ed6c4cafd72ea2ae66f76a8e39ee03d2
m3tro1d <qazwf734@gmail.com>
2021-09-13 20:23:49 +0300

    Add typescript treesitter config

--- lua/treesitter_config.lua
+++ lua/treesitter_config.lua
@@ -1,7 +1,7 @@
 require('nvim-treesitter.install').compilers = { 'clang', 'gcc' }
 
 require('nvim-treesitter.configs').setup({
-  ensure_installed = { 'python', 'c', 'cpp', 'lua', 'javascript', 'go', 'latex' },
+  ensure_installed = { 'python', 'c', 'cpp', 'lua', 'javascript', 'typescript', 'go', 'latex' },
   ignore_install = {},
   highlight = {
     enable = true,
