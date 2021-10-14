0b9e39a2c916cad18659b1a73d04636056dcc289
m3tro1d <qazwf734@gmail.com>
2021-10-14 14:22:23 +0300

    Autopairs tweaking

--- lua/autopairs_config.lua
+++ lua/autopairs_config.lua
@@ -1,6 +1,11 @@
 require('nvim-autopairs').setup({
   disable_filetype = { 'TelescopePrompt', 'vim' },
+  enable_moveright = true,
   enable_afterquote = false,
+  enable_check_bracket_line = true,
+  check_ts = false,
+  map_bs = true,
+  map_c_w = true,
 })
 
 require('nvim-autopairs.completion.compe').setup({
