9baa4178f0b1be2ae5839492397c81daabc9f535
m3tro1d <qazwf734@gmail.com>
2021-10-14 14:25:45 +0300

    Add resolve_timeout in compe

--- lua/compe_config.lua
+++ lua/compe_config.lua
@@ -6,6 +6,7 @@ require('compe').setup({
   preselect = 'enable',
   throttle_time = 80,
   source_timeout = 200,
+  resolve_timeout = 800,
   incomplete_delay = 400,
   max_abbr_width = 100,
   max_kind_width = 100,
