855db1a16cf92ee20f64aa03a599a8db381e36de
m3tro1d <qazwf734@gmail.com>
2021-10-21 17:27:46 +0300

    Add telescope mapping for git branches

--- lua/telescope_config.lua
+++ lua/telescope_config.lua
@@ -59,7 +59,12 @@ telescope.setup({
 telescope.load_extension('fzy_native')
 
 local opts = { noremap = true, silent = true }
+
 vim.api.nvim_set_keymap('n', '<C-p>', '<cmd>Telescope find_files<CR>', opts)
+
 vim.api.nvim_set_keymap('n', '<leader>fb', '<cmd>Telescope buffers<CR>', opts)
 vim.api.nvim_set_keymap('n', '<leader>fg', '<cmd>Telescope live_grep<CR>', opts)
+
 vim.api.nvim_set_keymap('n', '<leader>fh', '<cmd>Telescope help_tags<CR>', opts)
+
+vim.api.nvim_set_keymap('n', '<leader>gb', '<cmd>Telescope git_branches<CR>', opts)
