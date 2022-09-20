cf89c9f9d7d2f9c5f8473b46917dc57c08c76ab0
m3tro1d <qazwf734@gmail.com>
2022-09-20 19:46:20 +0300

    Add auto formatting in go

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -2,3 +2,13 @@ vim.opt_local.expandtab = false
 vim.opt_local.shiftwidth = 4
 vim.opt_local.tabstop = 4
 vim.opt_local.softtabstop = 4
+
+local group = vim.api.nvim_create_augroup('group', { clear = true })
+
+vim.api.nvim_create_autocmd('BufWritePre', {
+  group = group,
+  pattern = '*.go',
+  callback = function()
+    vim.lsp.buf.formatting()
+  end,
+})
