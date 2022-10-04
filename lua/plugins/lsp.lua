0580ed736cc66653902bbd3da8efa69e8814fe99
m3tro1d <qazwf734@gmail.com>
2022-10-04 21:10:59 +0300

    Remove clangd

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -45,7 +45,7 @@ local on_attach = function(client, bufnr)
   ]]
 end
 
-local servers = { 'clangd', 'tsserver', 'pylsp', 'gopls' }
+local servers = { 'tsserver', 'pylsp', 'gopls' }
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup {
     on_attach = on_attach,
