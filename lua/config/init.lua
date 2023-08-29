3169c67ccd91144b73153d9fcfc28eaa75b75f2d
nightnoryu <qazwf734@gmail.com>
2023-08-29 10:14:21 +0300

    Fix packer not being included

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -3,6 +3,7 @@ vim.opt.compatible = false
 vim.opt.encoding = 'utf-8'
 vim.cmd('filetype plugin indent on')
 
+require('config.packer')
 require('config.set')
 require('config.remap')
 require('config.gui')
