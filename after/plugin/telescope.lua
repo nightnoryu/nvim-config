94b54cdef18ce9ce6721575d81ee5c337f2f06b1
nightnoryu <qazwf734@gmail.com>
2023-10-05 23:49:34 +0300

    Include hidden files in Telescope search

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -60,7 +60,7 @@ telescope.setup({
 })
 telescope.load_extension('fzy_native')
 
-vim.api.nvim_set_keymap('n', '<C-p>', '<cmd>Telescope find_files<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-p>', '<cmd>Telescope find_files hidden=true<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>fb', '<cmd>Telescope buffers<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>fg', '<cmd>Telescope live_grep<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>fh', '<cmd>Telescope help_tags<CR>', { noremap = true, silent = true })
